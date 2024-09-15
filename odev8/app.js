9704c735fc3cee37512686b63158bb6a22af1bef
Fatma Aytekin <aytekin24@gmail.com>
2024-09-15 23:17:14 +0300

    Node.js Odev8 Tamamlandı.

---
+++ odev8/app.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const ejs=require('ejs');
+const path=require('path');
+const app=express();
+
+// template engine
+
+app.set('view engine','ejs');
+
+// Middleware
+app.use(express.static('public'));
+
+
+//routes
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+app.get('/about',(req,res)=>{
+    res.render('about');
+});
+app.get('/add_post',(req,res)=>{
+    res.render('add_post');
+});
+app.get('/post',(req,res)=>{
+    res.render('post');
+});
+
+const port=3000;
+app.listen(port,()=>{
+    console.log(`Sunucu ${port} portunda başlatıldı`);
+})
